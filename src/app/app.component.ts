fdebe1ec2395493c7649238026f306dac9b5d4cd
Lev <lyoha.bel2020@gmail.com>
2022-05-05 15:17:59 +0300

    Error: src/app/app.component.html

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Model } from "./repository.model";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angShablon';
+  model: Model = new Model();
+  // getClasses(): string {
+  //   return this.model.getProducts().length == 5 ? "bg-success" : "bg-warning";
+  // }
+  getClasses(key: number): string {
+    let product = this.model.getProduct(key);
+    // @ts-ignore
+    return "p-a-1 " + (product.price < 50 ? "bg-info" : "bg-warning");
+  }
 }
